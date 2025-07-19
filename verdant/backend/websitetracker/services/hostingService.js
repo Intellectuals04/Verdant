fa1b8fc9e5881f29b8b7ad0bb0747c6953fd201d
Tanishq <tanishqlokhande2005@gmail.com>
2025-07-19 10:28:06 +0530

    Recontruction

--- verdant/backend/websitetracker/services/hostingService.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-const { greenWebAPI } = require('../config/config');
-
-exports.checkGreenHosting = async (url) => {
-  const domain = new URL(url).hostname;
-  const { data } = await axios.get(`${greenWebAPI}${domain}`);
-  return data.green;
-};
