797b3a8bdb35414081500c2200994b0b305de8db
Sarvesh02Git <sarvesh02poffice@gmail.com>
2025-08-05 11:11:48 +0530

    DeletingExtraFiles

--- websiteTracker/pythonAI/websitetrackerai/as.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyBc_uZo4E-86M2ixV9tnPxa7ClzoNIA724")
-
-models = genai.list_models()
-
-for m in models:
-    print(f"{m.name} → {m.supported_generation_methods}")
